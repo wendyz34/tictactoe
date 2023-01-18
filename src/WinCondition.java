38e83c2735b313e76d05cf63520a74b2c5641da0
wendy <wendyz34@nycstudents.net>
2023-01-17 19:42:42 -0500

    Initial commit

---
+++ src/WinCondition.java
@@ -0,0 +1,20 @@
+public class WinCondition
+{
+    private int[] winningSpaces;
+    private String hey;
+
+    public WinCondition(int numWin)
+    {
+        if
+        winningSpaces = new int[3];
+        winningSpaces[0] = space1;
+        winningSpaces[1] = space2;
+        winningSpaces[2] = space3;
+    }
+
+
+    public int[] getWinningSpaces()
+    {
+        return winningSpaces;
+    }
+}
